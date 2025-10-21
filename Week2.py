5d1912e4b9127bc329ada7ad78f86836e595052d
Paolo957 <paolomaggiore1972@gmail.com>
2025-10-21 20:56:47 +0100

    Create Week2.py

---
+++ Week2.py
@@ -0,0 +1,15 @@
+print("welcome to week2")
+
+name = input("enter your name")
+print(f"hello {name}")
+
+a = 5
+b = 10
+print(f"the sum of {a} and {b} is {a + b}")
+
+text= input("enter your text:").strip()
+width=len(text) +4
+print("*"*width)
+print(f"*{text}*")
+print("*"*width)
+
