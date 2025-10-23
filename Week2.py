c1ff6cc51ac2e748be2464291db97c7c08673127
Paolo957 <paolomaggiore1972@gmail.com>
2025-10-23 20:55:54 +0100

    Update Week2.py

--- Week2.py
+++ Week2.py
@@ -13,3 +13,65 @@ print("*"*width)
 print(f"*{text}*")
 print("*"*width)
 
+title = "System Unit"
+dash = "_" * len(title)
+print(title)
+print(dash)
+
+phrase = "python is awesome"
+print(phrase.upper())
+print(phrase.capitalize())
+
+name = "Valentine"
+age = 20
+height = 5.9
+
+print ("name:", name)
+print("age:" , age)
+
+print (f" my name is {name} and I am {age} years old.")
+
+name = input("what your name?")
+print(f"your name is {name} and I am {age} years old.")
+print(f"next year you will be {age + 1} years old.")
+
+lives = int(input("please enter the number for lives: "))
+energy = int(input("please enter the number for energy: "))
+stemina = int(input("please enter the number for stemina: "))
+
+print("\nhealth has been set")
+print("lives:  ", " & " * lives)
+print("energy: ", " % " * energy)
+print("stemina:", " * " * stemina)
+
+num = int(input("please enter your number: "))
+if num > 0:
+    print("positive number")
+elif num < 0:
+    print("negative number")
+
+
+marks = int(input("please enter your marks"))
+if marks >= 80:
+    print("grade: A")
+
+elif marks >= 60:
+    print("grade: B")
+elif marks >= 40:
+    print("grade: C")
+else:
+    print("fail")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
