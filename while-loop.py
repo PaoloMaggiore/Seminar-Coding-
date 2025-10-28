7b2f3f0e74ec8ddd8c463783d3fa73e8d5921ebf
Paolo957 <paolomaggiore1972@gmail.com>
2025-10-28 20:51:16 +0000

    Create while-loop.py

---
+++ while-loop.py
@@ -0,0 +1,23 @@
+# number=int(input("enter a number"))
+# i = 1
+# while i < 11:
+#     print(number * i)
+#     i += 1
+
+
+# number=int(input("enter a number"))
+# i = 1
+# while i < 11:
+#     print(number, "*", i, "=", number * i)
+#     i += 1
+
+
+x=1
+while x < 5:
+    print(x)
+    x += 1
+else:
+    print('limit reached')
+
+
+
