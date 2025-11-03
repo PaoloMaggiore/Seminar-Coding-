2f76d830197442bf173dd2af320b99f4869f2109
Paolo957 <paolomaggiore1972@gmail.com>
2025-11-03 19:18:57 +0000

    Update while-loop.py

--- while-loop.py
+++ while-loop.py
@@ -19,13 +19,95 @@
 # else:
 #     print('limit reached')
 
-total_num= int(input("how many obstacles must be avoid"))
-avoid = 0
+# total_num= int(input("how many obstacles must be avoid"))
+# avoid = 0
+#
+# while avoid < total_num:
+#     print("avoiding ..." , end=" ")
+#     avoid += 1
+#     print(avoid)
 
-while avoid < total_num:
-    print("avoiding ..." , end=" ")
-    avoid += 1
-    print(avoid)
+# 1. Print Multiplication Table
+# number = int(input("Enter the number: "))
+# i = 1
+# while i < 11:
+#     print(number * i)
+#     i += 1
+
+
+
+# num_apples=int(input("How many apples should I remove?"))
+# removed=0
+# while removed<num_apples:
+#     print("Apple",removed)
+#     removed+=1
+
+
+# num_obstacles=int(input("How many obstacles should I remove?"))
+# removed=0
+# while avoid<num_obstacles:
+#     print("Avoiding",removed)
+#     removed+=1
+#     print(f" ....Done {avoid} obstacles avoided")
+#     print("all the obstacles are avoided"))      check this code 6
+
+
+
+# num_bars=int(input("How many bars should be charged??"))
+# charged =0
+# while charged<num_bars:
+#     print("Charging: ....")
+#     charged+=1
+#     print(f"{charged} Bar charged")
+# print("the battery is fully charged")
+
+
+# for count in range(100):
+#     print("#########")
+#     print("#       #")
+#     print("# O   O #")
+#     print("|   V   |")
+#     print("|  ---  |")
+#     print("|_______|")
+
+# num_montains = int(input("How many mountains should I display?"))
+# print("\nDisplaying ....\n")
+# for var1 in range(num_montains):
+#     print("*")
+#     print("**")
+#     print("***")
+#     print("****")
+#     print("*****")
+#     print("******")
+
+
+# number = int(input("Enter the number: "))
+# i = 1
+# for i in range (1,20):
+#
+#      print(number * i)
+#      i += 1
+
+
+
+# steps = int(input("How far are we from the target?"))
+# for i in range(steps,0,-1):
+#     print(i,"steps remaining")
+
+
+# print("Please enter a word")
+# user_word = input()
+#
+# for position in range(0, len(user_word), 1):
+#     print(user_word[position])
+
+
+rows=int(input("Enter the number of rows: "))
+cols=int(input("Enter the number of columns: "))
+for i in range(rows):
+    for j in range(cols):
+        print(":-)",end="")
+    print()
 
 
 
